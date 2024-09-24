6354a0e8db1f62076231b8b4443d493dc433177b
tsiotska <vitaliytsisaruk@gmail.com>
2024-09-24 16:40:11 +0300

    Refactor: variables.

--- pi_challenge_2023.js
+++ pi_challenge_2023.js
@@ -8,55 +8,52 @@
 function solution(P, Q) {
   if (P === Q) return new Set(P).size
 
-  const commonFrequency = new Map();
-  const frequencyP = new Map();
-  const frequencyQ = new Map();
+  const commonFrequencyMap = new Map();
+  const frequencyPMap = new Map();
+  const frequencyQMap = new Map();
 
   for (let i = 0; i < P.length; i++) {
-    frequencyP.set(P[i], (frequencyP.get(P[i]) || 0) + 1)
-    frequencyQ.set(Q[i], (frequencyQ[Q[i]] || 0) + 1);
-    if (P[i] === Q[i]) commonFrequency.set(P[i], (commonFrequency.get(P[i]) || 0) + 1);
-    else {
-      commonFrequency.set(P[i], (commonFrequency.get(P[i]) || 0) + 1);
-      commonFrequency.set(Q[i], (commonFrequency.get(Q[i]) || 0) + 1);
+    frequencyPMap.set(P[i], (frequencyPMap.get(P[i]) || 0) + 1)
+    frequencyQMap.set(Q[i], (frequencyQMap[Q[i]] || 0) + 1);
+    commonFrequencyMap.set(P[i], (commonFrequencyMap.get(P[i]) || 0) + 1);
+    if (P[i] !== Q[i]) {
+      commonFrequencyMap.set(Q[i], (commonFrequencyMap.get(Q[i]) || 0) + 1);
     }
   }
 
   const result = []
 
-  function branch(S, addLetter, unusedLetter, frequencyPCopy, frequencyQCopy, commonFrequencyCopy, i) {
+  function branch(S, addLetter, unusedLetter, commonFrequencyMapCopy, i) {
     const newSet = new Set(S);
     newSet.add(addLetter);
-    frequencyPCopy.set(unusedLetter, frequencyPCopy.get(unusedLetter) - 1);
-    commonFrequencyCopy.set(unusedLetter, commonFrequencyCopy.get(unusedLetter) - 1);
-    helper(commonFrequencyCopy, frequencyPCopy, frequencyQCopy, S, i + 1);
+    commonFrequencyMapCopy.set(unusedLetter, commonFrequencyMapCopy.get(unusedLetter) - 1);
+    helper(commonFrequencyMapCopy, newSet, i + 1);
   }
 
-  function helper(commonFrequency, frequencyP, frequencyQ, S, j = 0) {
-    const commonFrequencyCopy = new Map(commonFrequency),
-      frequencyPCopy = new Map(frequencyP),
-      frequencyQCopy = new Map(frequencyQ);
+  function helper(commonFrequency, S, j = 0) {
+    const commonFrequencyMapCopy = new Map(commonFrequency);
 
     for (let i = j; i < P.length; i++) {
+      const frequencyQ = commonFrequencyMapCopy.get(Q[i])
+      const frequencyP = commonFrequencyMapCopy.get(P[i])
+
       // Priority for already added letters
       if (S.has(P[i])) {
         S.add(P[i]);
-        frequencyQCopy.set(Q[i], frequencyQCopy.get(Q[i]) - 1);
-        commonFrequencyCopy.set(Q[i], commonFrequencyCopy.get(Q[i]) - 1);
+        commonFrequencyMapCopy.set(Q[i], frequencyQ - 1);
         continue;
       }
       if (S.has(Q[i])) {
         S.add(Q[i]);
-        frequencyQCopy.set(P[i], frequencyQCopy.get(P[i]) - 1);
-        commonFrequencyCopy.set(P[i], commonFrequencyCopy.get(P[i]) - 1);
+        commonFrequencyMapCopy.set(P[i], frequencyP - 1);
         continue;
       }
 
       // Same priority leads to branching
-      if (commonFrequencyCopy.get(P[i]) === commonFrequencyCopy.get(Q[i])) {
+      if (frequencyP === frequencyQ) {
         if (P[i] !== Q[i]) {
-          branch(S, P[i], Q[i], frequencyPCopy, frequencyQCopy, commonFrequencyCopy, i);
-          branch(S, Q[i], P[i], frequencyPCopy, frequencyQCopy, commonFrequencyCopy, i);
+          branch(S, P[i], Q[i], commonFrequencyMapCopy, i);
+          branch(S, Q[i], P[i], commonFrequencyMapCopy, i);
           return null;
         }
 
@@ -66,35 +63,33 @@ function solution(P, Q) {
 
       // BRANCH IF less frequent Q is present in both strings while P is not
       // Otherwise use more frequent P
-      if (commonFrequencyCopy.get(P[i]) > commonFrequencyCopy.get(Q[i])) {
-        if (frequencyPCopy.get(Q[i]) && !frequencyQCopy.get(P[i])) {
-          branch(S, Q[i], P[i], frequencyPCopy, frequencyQCopy, commonFrequencyCopy, i);
+      if (frequencyP > frequencyQ) {
+        if (frequencyPMap.get(Q[i]) && !frequencyQMap.get(P[i])) {
+          branch(S, Q[i], P[i], commonFrequencyMapCopy, i);
           return null;
         }
 
         // Add P and remove unused Q from frequency
         S.add(P[i]);
-        frequencyQCopy.set(Q[i], frequencyQCopy.get(Q[i]) - 1);
-        commonFrequencyCopy.set(Q[i], commonFrequencyCopy.get(Q[i]) - 1);
+        commonFrequencyMapCopy.set(Q[i], frequencyQ - 1);
       }
-        // BRANCH IF less frequent P is present in both strings while Q is not
+      // BRANCH IF less frequent P is present in both strings while Q is not
       // Otherwise use more frequent Q
       else {
-        if (frequencyQCopy.get(P[i]) && !frequencyPCopy.get(Q[i])) {
-          branch(S, P[i], Q[i], frequencyPCopy, frequencyQCopy, commonFrequencyCopy, i);
+        if (frequencyQMap.get(P[i]) && !frequencyPMap.get(Q[i])) {
+          branch(S, P[i], Q[i], commonFrequencyMapCopy, i);
           return null;
         }
         // Add Q and remove unused P from frequency
         S.add(Q[i]);
-        frequencyPCopy.set(P[i], frequencyPCopy.get(P[i]) - 1);
-        commonFrequencyCopy.set(P[i], commonFrequencyCopy.get(P[i]) - 1);
+        commonFrequencyMapCopy.set(P[i], frequencyP - 1);
       }
     }
     console.log(S)
     result.push(S.size)
   }
 
-  helper(commonFrequency, frequencyP, frequencyQ, new Set())
+  helper(commonFrequencyMap, new Set())
   return Math.min(...result);
 }
 
@@ -102,9 +97,9 @@ const testCases = [
   ["dddabc", "abcefg"], // abcabc - expected (min distinct letters num equals 3)
   ["aaaacbcddd", "bbbbacdaaa"], // aaaaaccaaa - expected (2)
   ["abcdfghiklmnpq", "bcdeghijlmnoqr"], // bbddggiillnnqq - expected (7)
-  ["aaabbbcccdeh", "befceidejehi"], // expected (4) TODO: cover case
+  ["aaabbbcccdeh", "befceidejehi"], // aaaceiccceei expected (4) TODO: cover case
   ["aaaabddddefi", "cghjgeefhjgj"], // aaaagddddjgj - expected (5) TODO: cover case
-  ["aabbbddeefggi", "dhcgjgifjgijj"]
+  ["aabbbddeefggi", "dhcgjgifjgijj"] // aabbbgieegigi - expected (5) TODO: cover case
 ]
 
  testCases.forEach((test) => {
