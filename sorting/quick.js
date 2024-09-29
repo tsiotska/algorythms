4b87388f2c7d13d869176e5835902582e0430814
tsiotska <vitaliytsisaruk@gmail.com>
2024-09-29 20:18:11 +0300

    Quick sort.

---
+++ sorting/quick.js
@@ -0,0 +1,20 @@
+const quickSort = (arr) => {
+  if (arr.length <= 1) return arr;
+
+  const pivot = arr[Math.floor(arr.length / 2)];
+  const left = [];
+  const right = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (i === Math.floor(arr.length / 2)) continue;
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+
+  return [...quickSort(left), pivot, ...quickSort(right)];
+}
+
+console.log(quickSort([34, 7, 23, 32, 5, 62]));
